d9b9689fefcb2bcd85c5811f4d5d4c3d81ccab23
nahuakang <kangnahua@gmail.com>
2020-08-28 14:39:28 +0200

    Timestamp 6.3: Add bootstrap template and dynamic yields

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/nahuakang/gophotos/views"
+)
+
+var homeView *views.View
+var contactView *views.View
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	err := homeView.Template.ExecuteTemplate(w, homeView.Layout, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	err := contactView.Template.ExecuteTemplate(w, contactView.Layout, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	homeView = views.NewView("bootstrap", "views/home.gohtml")
+	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+
+	http.ListenAndServe(":3000", r)
+}
