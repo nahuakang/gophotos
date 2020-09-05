fab5adfc39b73dc5ac504ba96f1a0cfc3f0de823
nahuakang <kangnahua@gmail.com>
2020-09-05 13:38:42 +0200

    Add automigrate method; refactor destructive reset

--- models/users.go
+++ models/users.go
@@ -96,9 +96,21 @@ func (us *UserService) ByEmail(email string) (*User, error) {
 }
 
 // DestructiveReset drops the user table and rebuilds it
-func (us *UserService) DestructiveReset() {
-	us.db.DropTableIfExists(&User{})
-	us.db.AutoMigrate(&User{})
+func (us *UserService) DestructiveReset() error {
+	err := us.db.DropTableIfExists(&User{}).Error
+	if err != nil {
+		return err
+	}
+	return us.AutoMigrate()
+}
+
+// AutoMigrate will attempt to automatically migrate user table
+func (us *UserService) AutoMigrate() error {
+	if err := us.db.AutoMigrate(&User{}).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // Close closes UserService database connection
