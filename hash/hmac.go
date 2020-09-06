8d3d1578f7efd6dd8d83f4b49f94dcae87f61087
nahuakang <kangnahua@gmail.com>
2020-09-06 20:39:03 +0200

    Add hash package for HMAC

---
+++ hash/hmac.go
@@ -0,0 +1,31 @@
+package hash
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"hash"
+)
+
+// NewHMAC creates and returns a new HMAC object
+func NewHMAC(key string) HMAC {
+	h := hmac.New(sha256.New, []byte(key))
+	return HMAC{
+		hmac: h,
+	}
+}
+
+// HMAC is a wrapper around the crypto/hmac package
+// making it easier to use in the web app
+type HMAC struct {
+	hmac hash.Hash
+}
+
+// Hash hashes the provided input string using HMAC with
+// the secret key provided when HMAC object was created.
+func (h HMAC) Hash(input string) string {
+	h.hmac.Reset()
+	h.hmac.Write([]byte(input))
+	b := h.hmac.Sum(nil)
+	return base64.URLEncoding.EncodeToString(b)
+}
