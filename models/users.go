e6a484ddbc3c4b77f487777dea7350936ee83d12
nahuakang <kangnahua@gmail.com>
2020-09-06 20:41:55 +0200

    Update User struct for models

--- models/users.go
+++ models/users.go
@@ -40,6 +40,8 @@ type User struct {
 	Email        string `gorm:"not null;unique_index"`
 	Password     string `gorm:"-"`
 	PasswordHash string `gorm:"not null"`
+	Remember     string `gorm:"-"`
+	RememberHash string `gorm:"not null;unique_index"`
 }
 
 // UserService is an abstract layer to interact with gorm.DB
