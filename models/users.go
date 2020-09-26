c8c30ba98b2c9c48907bdab8639e669c8b1b8c9e
nahuakang <kangnahua@gmail.com>
2020-09-26 17:30:46 +0200

    Add hmacRemember helper to refactor validation code

--- models/users.go
+++ models/users.go
@@ -159,41 +159,61 @@ func (uv *userValidator) bcryptPassword(user *User) error {
 	return nil
 }
 
+// hmacRemember is a helper function to be consumed by userValidator.ByRemember
+func (uv *userValidator) hmacRemember(user *User) error {
+	if user.Remember == "" {
+		return nil
+	}
+	user.RememberHash = uv.hmac.Hash(user.Remember)
+	return nil
+}
+
 // ByRemember hashes the remember token and calls
 // ByRemember on the subsequent UserDB layer.
 func (uv *userValidator) ByRemember(token string) (*User, error) {
-	rememberHash := uv.hmac.Hash(token)
-	return uv.UserDB.ByRemember(rememberHash)
+	user := User{
+		Remember: token,
+	}
+	if err := runUserValFns(&user, uv.hmacRemember); err != nil {
+		return nil, err
+	}
+
+	return uv.UserDB.ByRemember(user.RememberHash)
 }
 
 // Create creates the provided user and backfills data
 // such as ID, CreateAt, and UpdateAt fields.
 func (uv *userValidator) Create(user *User) error {
-	if err := runUserValFns(user, uv.bcryptPassword); err != nil {
-		return err
-	}
-
 	if user.Remember == "" {
 		token, err := rand.RememberToken()
 		if err != nil {
-			return err
 		}
 		user.Remember = token
 	}
 
-	user.RememberHash = uv.hmac.Hash(user.Remember)
+	err := runUserValFns(
+		user,
+		uv.bcryptPassword,
+		uv.hmacRemember,
+	)
+	if err != nil {
+		return err
+	}
+
 	return uv.UserDB.Create(user)
 }
 
 // Update hashes a remember token if one is provided
 func (uv *userValidator) Update(user *User) error {
-	if err := runUserValFns(user, uv.bcryptPassword); err != nil {
+	err := runUserValFns(
+		user,
+		uv.bcryptPassword,
+		uv.hmacRemember,
+	)
+	if err != nil {
 		return err
 	}
 
-	if user.Remember != "" {
-		user.RememberHash = uv.hmac.Hash(user.Remember)
-	}
 	return uv.UserDB.Update(user)
 }
 
