fe1540d87a6f133e885311a12c39654b0a12d773
nahuakang <kangnahua@gmail.com>
2020-10-23 15:56:58 +0200

    14.2 Create GalleryService

--- models/galleries.go
+++ models/galleries.go
@@ -2,6 +2,25 @@ package models
 
 import "github.com/jinzhu/gorm"
 
+// GalleryService is an interface that represents services to Gallery
+type GalleryService interface {
+	GalleryDB
+}
+
+// GalleryDB represents interface for the gallery database
+type GalleryDB interface {
+	Create(gallery *Gallery) error
+}
+
+type galleryGorm struct {
+	db *gorm.DB
+}
+
+func (gg *galleryGorm) Create(gallery *Gallery) error {
+	// TODO: Implement later
+	return nil
+}
+
 // Gallery represents the model for a user gallery
 type Gallery struct {
 	gorm.Model
