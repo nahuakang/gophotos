249fbf6002f3a09a70de819d5d69c262e7884d75
nahuakang <kangnahua@gmail.com>
2020-10-25 10:54:35 +0100

    14.5.1 Implement the gallery service

--- models/galleries.go
+++ models/galleries.go
@@ -2,8 +2,30 @@ package models
 
 import "github.com/jinzhu/gorm"
 
+// Gallery represents the model for a user gallery
+type Gallery struct {
+	gorm.Model
+	UserID uint   `gorm:"not_null;index"`
+	Title  string `gorm:"not_null"`
+}
+
+// NewGalleryService returns a GalleryService
+func NewGalleryService(db *gorm.DB) GalleryService {
+	return &galleryService{
+		GalleryDB: &galleryValidator{
+			GalleryDB: &galleryGorm{
+				db: db,
+			},
+		},
+	}
+}
+
 // GalleryService is an interface that represents services to Gallery
 type GalleryService interface {
+	GalleryDB
+}
+
+type galleryService struct {
 	GalleryDB
 }
 
@@ -12,18 +34,17 @@ type GalleryDB interface {
 	Create(gallery *Gallery) error
 }
 
+type galleryValidator struct {
+	GalleryDB
+}
+
+// Ensure galleryGorm implements GalleryDB interface
+var _ GalleryDB = &galleryGorm{}
+
 type galleryGorm struct {
 	db *gorm.DB
 }
 
 func (gg *galleryGorm) Create(gallery *Gallery) error {
-	// TODO: Implement later
-	return nil
-}
-
-// Gallery represents the model for a user gallery
-type Gallery struct {
-	gorm.Model
-	UserID uint   `gorm:"not_null;index"`
-	Title  string `gorm:"not_null"`
+	return gg.db.Create(gallery).Error
 }
