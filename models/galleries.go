c00df200432c6ab42890aaba40a8bdd2d71ced0f
nahuakang <kangnahua@gmail.com>
2020-10-25 13:34:02 +0100

    14.5.4 Validate galleries

--- models/galleries.go
+++ models/galleries.go
@@ -2,6 +2,13 @@ package models
 
 import "github.com/jinzhu/gorm"
 
+const (
+	// ErrUserIDRequired is returned if user ID is not present
+	ErrUserIDRequired modelError = "models: user ID is required"
+	// ErrTitleRequired is returned if gallery title is not present
+	ErrTitleRequired modelError = "models: title is required"
+)
+
 // Gallery represents the model for a user gallery
 type Gallery struct {
 	gorm.Model
@@ -38,6 +45,34 @@ type galleryValidator struct {
 	GalleryDB
 }
 
+// Create creates gallery
+func (gv *galleryValidator) Create(gallery *Gallery) error {
+	err := runGalleryValFns(
+		gallery,
+		gv.userIDRequired,
+		gv.titleRequired,
+	)
+	if err != nil {
+		return err
+	}
+
+	return gv.GalleryDB.Create(gallery)
+}
+
+func (gv *galleryValidator) userIDRequired(g *Gallery) error {
+	if g.UserID <= 0 {
+		return ErrUserIDRequired
+	}
+	return nil
+}
+
+func (gv *galleryValidator) titleRequired(g *Gallery) error {
+	if g.Title == "" {
+		return ErrTitleRequired
+	}
+	return nil
+}
+
 // Ensure galleryGorm implements GalleryDB interface
 var _ GalleryDB = &galleryGorm{}
 
@@ -47,4 +82,16 @@ type galleryGorm struct {
 
 func (gg *galleryGorm) Create(gallery *Gallery) error {
 	return gg.db.Create(gallery).Error
+}
+
+type galleryValFn func(*Gallery) error
+
+func runGalleryValFns(gallery *Gallery, fns ...galleryValFn) error {
+	for _, fn := range fns {
+		if err := fn(gallery); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
